ff9d863952440333d467e090880e685d2392556f
Igor Cezatte <igorcezatte13@gmail.com>
2020-07-01 18:48:42 -0300

    fix width button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,12 +2,13 @@ import styled from 'styled-components/native';
 import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled(RectButton)`
-  width: 100%;
+  width: 330px;
   height: 60px;
   background: #ff9000;
   border-radius: 10px;
   margin-top: 8px;
 
+  margin: auto;
   justify-content: center;
   align-items: center;
 `;
