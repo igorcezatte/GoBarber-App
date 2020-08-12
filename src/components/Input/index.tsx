03d444cdf1a90570bfbb8af9afff26e9efb6e3da
Igor Cezatte <igorcezatte13@gmail.com>
2020-08-11 23:57:09 -0300

    profile page structured

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -14,6 +14,7 @@ import { Container, TextInput, Icon } from './styles';
 interface InputProps extends TextInputProps {
   name: string;
   icon: string;
+  containerStyle?: {};
 }
 
 interface InputValueReference {
@@ -25,7 +26,7 @@ interface InputRef {
 }
 
 const Input: React.RefForwardingComponent<InputRef, InputProps> = (
-  { name, icon, ...rest },
+  { name, icon, containerStyle = {}, ...rest },
   ref,
 ) => {
   const inputElementRef = useRef<any>(null);
@@ -73,7 +74,7 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   }, [fieldName, registerField]);
 
   return (
-    <Container isErrored={!!error} isFocused={isFocused}>
+    <Container style={containerStyle} isErrored={!!error} isFocused={isFocused}>
       <Icon
         name={icon}
         size={20}
